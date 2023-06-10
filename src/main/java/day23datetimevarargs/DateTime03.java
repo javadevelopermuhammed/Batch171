1baa5b2e8067914b9712ef414d45f1d60073778b
Muhammed Emin Kaba <m.eminkaba0000@gmail.com>
2023-06-10 23:24:39 +0300

    Day24

---
+++ src/main/java/day23datetimevarargs/DateTime03.java
@@ -0,0 +1,13 @@
+package day23datetimevarargs;
+
+import java.time.LocalDateTime;
+
+public class DateTime03 {
+
+    public static void main(String[] args) {
+
+
+        LocalDateTime ldt = LocalDateTime.now();
+        System.out.println(ldt);
+    }
+}

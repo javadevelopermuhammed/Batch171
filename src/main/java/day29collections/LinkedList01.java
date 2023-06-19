f893f3581f585d2cc5692339102686445562fe0c
Muhammed Emin Kaba <m.eminkaba0000@gmail.com>
2023-06-19 14:08:06 +0300

    Day29

---
+++ src/main/java/day29collections/LinkedList01.java
@@ -0,0 +1,17 @@
+package day29collections;
+
+import java.util.LinkedList;
+
+public class LinkedLists01 {
+    public static void main(String[] args) {
+        LinkedList<String> s = new LinkedList<>();
+        s.add("Nuriye");
+        s.add("Orhan");
+        s.add("Bunyamin");
+        s.add("Saadet");
+        s.add("Niyazi");
+        s.add("Bilal");
+        s.add("Abdullah");
+        s.add("Nuriye");
+    }
+}

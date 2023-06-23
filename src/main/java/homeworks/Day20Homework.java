50605b5dd590d47f172c84e70c94615ff7203f0f
Muhammed Emin Kaba <m.eminkaba0000@gmail.com>
2023-06-23 23:22:33 +0300

    Day31

--- src/main/java/homeworks/Day20Homework.java
+++
@@ -1,2 +0,0 @@
-package homeworks;public class Day20Homework {
-}
---
+++ src/main/java/homeworks/Day30Homework.java
@@ -0,0 +1,11 @@
+package homeworks;
+
+public class Day20Homework {
+    public static void main(String[] args) {
+        /*
+        Home work:
+        Verilen bir String'de hangi harfin kac defa kullanildigini veren method olusturunuz.
+        "Hello Henry!" ==> H=2, e=2, l=2, o=1, n=1, r=1, y=1
+         */
+    }
+}
